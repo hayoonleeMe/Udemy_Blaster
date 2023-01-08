d36eb56822b413924b178ffcf4f801e8179d6ae3
hayoonlee <htdobad@gmail.com>
2023-01-08 15:14:30 +0900

    Match States - Match State

--- Source/Blaster/GameMode/BlasterGameMode.cpp
+++ Source/Blaster/GameMode/BlasterGameMode.cpp
@@ -9,6 +9,36 @@
 #include "Blaster/PlayerState/BlasterPlayerState.h"
 #include "Components/SlateWrapperTypes.h"
 
+ABlasterGameMode::ABlasterGameMode()
+{
+	bDelayedStart = true;
+}
+
+void ABlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LevelStartingTime = GetWorld()->GetTimeSeconds();
+}
+
+void ABlasterGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (MatchState == MatchState::WaitingToStart)
+	{
+		// GetWorld()->GetTimeSeconds() 은 게임을 켤 때 부터 시간을 재기 때문에 Match 를 시작해 BlasterMap 으로 들어올 때 이미 이 시간은 0이 아니다.
+		// 따라서 BlasterMap 으로 들어왔을 때의 시간인 LevelStartingTime 을 BeginPlay 에서 구한다.
+		// CountdownTime 은 앞으로 남은 시간을 의미하고, LevelStartingTime 을 더해 이미 더 많이 지난 시간을 보충한다.
+		CountdownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime;
+
+		if (CountdownTime <= 0.f)
+		{
+			StartMatch();
+		}
+	}
+}
+
 void ABlasterGameMode::PlayerEliminated(ABlasterCharacter* ElimmedCharacter, ABlasterPlayerController* VictimController,
                                         ABlasterPlayerController* AttackerController)
 {
