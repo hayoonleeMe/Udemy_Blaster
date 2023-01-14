2e19836a3a3666c4fdcb8ed7374767f0eb0da815
hayoonlee <htdobad@gmail.com>
2023-01-14 18:36:56 +0900

    Different Weapon Types - Spawning Rocket Trails

--- Source/Blaster/Weapon/ProjectileRocket.cpp
+++ Source/Blaster/Weapon/ProjectileRocket.cpp
@@ -3,6 +3,11 @@
 
 #include "ProjectileRocket.h"
 #include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include "Sound/SoundCue.h"
+#include "Components/BoxComponent.h"
+#include "Components/AudioComponent.h"
 
 AProjectileRocket::AProjectileRocket()
 {
@@ -11,12 +16,38 @@ AProjectileRocket::AProjectileRocket()
 	RocketMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
+void AProjectileRocket::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 클라이언트에도 Hit Event 등록
+	if (!HasAuthority())
+	{
+		CollisionBox->OnComponentHit.AddDynamic(this, &AProjectileRocket::OnHit);
+	}
+
+	if (TrailSystem)
+	{
+		TrailSystemComponent = UNiagaraFunctionLibrary::SpawnSystemAttached(TrailSystem, GetRootComponent(), FName(), GetActorLocation(), GetActorRotation(), EAttachLocation::KeepWorldPosition, false);
+	}
+
+	if (ProjectileLoop && LoopingSoundAttenuation)
+	{
+		ProjectileLoopComponent = UGameplayStatics::SpawnSoundAttached(ProjectileLoop, GetRootComponent(), FName(), GetActorLocation(), EAttachLocation::KeepWorldPosition, false, 1.f, 1.f, 0.f, LoopingSoundAttenuation, nullptr, false);
+	}
+}
+
+void AProjectileRocket::DestroyTimerFinished()
+{
+	Destroy();
+}
+
 void AProjectileRocket::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
                               FVector NormalImpulse, const FHitResult& Hit)
 {
 	// Instigator : 액터의 피해를 담당하는 APawn, Projectile 을 Weapon 에서 생성할 때 Weapon 의 Owner 를 Projectile 의 Instigator 로 설정하여 생성함
 	APawn* FiringPawn = GetInstigator();
-	if (FiringPawn)
+	if (FiringPawn && HasAuthority())	// 클라이언트에도 Hit Event가 발생하므로 OnHit의 데미지 관련 로직은 서버에서만 수행하도록 설정
 	{
 		AController* FiringController = FiringPawn->GetController();
 		if (FiringController)
@@ -25,6 +56,37 @@ void AProjectileRocket::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, 
 			UGameplayStatics::ApplyRadialDamageWithFalloff(this, Damage, 10.f, GetActorLocation(), 200.f, 500.f, 1.f, UDamageType::StaticClass(), TArray<AActor*>(), this, FiringController);
 		}
 	}
-	
-	Super::OnHit(HitComp, OtherActor, OtherComp, NormalImpulse, Hit);
+
+	GetWorldTimerManager().SetTimer(DestroyTimer, this, &AProjectileRocket::DestroyTimerFinished, DestroyTime);
+
+	// 부모 클래스는 Projectile 액터가 Destroy 될 때 Impact 효과를 주지만, ProjectileRocket 액터가 3초 뒤에 Destroy 되므로 Hit Event 가 발생해 OnHit()가 호출될 때 Impact 효과를 주도록 설정  
+	if (ImpactParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles, GetActorTransform());
+	}
+	if (ImpactSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+	}
+	if (RocketMesh)
+	{
+		RocketMesh->SetVisibility(false);
+	}
+	if (CollisionBox)
+	{
+		CollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+	if (TrailSystemComponent && TrailSystemComponent->GetSystemInstanceController())
+	{
+		TrailSystemComponent->GetSystemInstanceController()->Deactivate();
+	}
+	if (ProjectileLoopComponent && ProjectileLoopComponent->IsPlaying())
+	{
+		ProjectileLoopComponent->Stop();
+	}
+}
+
+void AProjectileRocket::Destroyed()
+{
+	// Super::Destroyed()를 비호출하여 3초 뒤에 Destroyed 될 때 Impact 효과를 표시하지 않도록 함
 }
