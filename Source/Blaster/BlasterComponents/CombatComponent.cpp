2b1cd62aad25ffab4b4b9a8d5a14a39c96d1cecd
hayoonlee <htdobad@gmail.com>
2023-01-22 14:49:08 +0900

    Different Weapon Types - Grenades in Multiplayer

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -347,11 +347,18 @@ void UCombatComponent::ThrowGrenadeFinished()
 void UCombatComponent::LaunchGrenade()
 {
 	ShowAttachedGrenade(false);
+	if (Character && Character->IsLocallyControlled())
+	{
+		ServerLaunchGrenade(HitTarget);
+	}
+}
 
-	if (Character && Character->HasAuthority() && GrenadeClass && Character->GetAttachedGrenade())
+void UCombatComponent::ServerLaunchGrenade_Implementation(const FVector_NetQuantize& Target)
+{
+	if (Character && GrenadeClass && Character->GetAttachedGrenade())
 	{
 		const FVector StartingLocation = Character->GetAttachedGrenade()->GetComponentLocation();
-		FVector ToTarget = HitTarget - StartingLocation;
+		FVector ToTarget = Target - StartingLocation;
 		
 		FActorSpawnParameters SpawnParams;
 		SpawnParams.Owner = Character;
