2822775d8adab46df228b651d4e70c0968e057b6
hayoonlee <htdobad@gmail.com>
2023-01-26 15:17:07 +0900

    Pickups - Pickup Class

---
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Components/SphereComponent.h"
+
+
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	// Root Component를 모든 Actor 클래스에게 상속되는 USceneComponent로 설정한다. 이를 통해 다른 모든 컴포넌트를 부착할 수 있도록 한다.
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetSphereRadius(150.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->SetupAttachment(OverlapSphere);
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 서버에서만 Overlap Event 발생
+	if (HasAuthority())
+	{
+		// Overlap Event 등록
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);	
+	}
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if (PickupSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+	}
+}
