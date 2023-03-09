f1bbd2bcb1fb85f1c065c0ee9f992cb5ebd0c8fd
hayoonlee <htdobad@gmail.com>
2023-03-09 20:41:16 +0900

    More Multiplayer Features - Projectile Head Shots

--- Source/Blaster/Weapon/ProjectileWeapon.cpp
+++ Source/Blaster/Weapon/ProjectileWeapon.cpp
@@ -32,6 +32,7 @@ void AProjectileWeapon::Fire(const FVector& HitTarget)
  					SpawnedProjectile = World->SpawnActor<AProjectile>(ProjectileClass, SocketTransform.GetLocation(), TargetRotation, SpawnParams);
  					SpawnedProjectile->bUseServerSideRewind = false;
  					SpawnedProjectile->Damage = Damage;
+ 					SpawnedProjectile->HeadShotDamage = HeadShotDamage;
  				}
  				else // server, not locally controlled - spawn non-replicated projectile, SSR
  				{
@@ -47,7 +48,7 @@ void AProjectileWeapon::Fire(const FVector& HitTarget)
  					SpawnedProjectile->bUseServerSideRewind = true;
  					SpawnedProjectile->TraceStart = SocketTransform.GetLocation();
  					SpawnedProjectile->InitialVelocity = SpawnedProjectile->GetActorForwardVector() * SpawnedProjectile->InitialSpeed;
- 					SpawnedProjectile->Damage = Damage;
+ 					//SpawnedProjectile->Damage = Damage; 제거
  				}
  				else // client, not locally controlled - spawn non-replicated projectile, no SSR
  				{
@@ -63,6 +64,7 @@ void AProjectileWeapon::Fire(const FVector& HitTarget)
  				SpawnedProjectile = World->SpawnActor<AProjectile>(ProjectileClass, SocketTransform.GetLocation(), TargetRotation, SpawnParams);
  				SpawnedProjectile->bUseServerSideRewind = false;
  				SpawnedProjectile->Damage = Damage;
+ 				SpawnedProjectile->HeadShotDamage = HeadShotDamage;
  			}
  		}
  	}
